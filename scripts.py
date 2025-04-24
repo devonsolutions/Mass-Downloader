1507284aac3e50b791e97f6bde515c98969c23b7
CacheOutDev <cache-out@outlook.com>
2025-04-24 14:41:29 -0700

    Added an elif statement for spreadsheets

--- scripts.py
+++ scripts.py
@@ -19,16 +19,33 @@ for fullnameFile in fullnameFiles:
 
     if response.status_code == 200:
 
-        # Maybe add another ifelse statement so that we can download status code [200] Drupal7 files, as well
+        if "/sites/default/files" and ".pdf" in fullnameFile:
 
-        pattern = "https://www.csustan.edu/sites/default/files/" + '[0-9]+' + '-' + '[0-9]+' + '/'
-        repl = ''
-        removeURL = re.sub(pattern, repl, fullnameFile)
-        filePath = "Downloads" + str("/") + removeURL
+            # Maybe add another ifelse statement so that we can download status code [200] Drupal7 files, as well
 
-        open(filePath, "wb").write(response.content)
+            pattern = "https://www.csustan.edu/sites/default/files/" + '[0-9]+' + '-' + '[0-9]+' + '/'
+            repl = ''
+            removeURL = re.sub(pattern, repl, fullnameFile)
+            filePath = "Downloads" + str("/") + removeURL
 
-        print(str(filePath) + "has been downloaded.")
+            open(filePath, "wb").write(response.content)
+
+            print(str(filePath) + "has been downloaded.")
+        
+        elif ".xlsx" in fullnameFile:
+            # Maybe add another ifelse statement so that we can download status code [200] Drupal7 files, as well
+
+            pattern = "https://www.csustan.edu/sites/default/files/" + '[0-9]+' + '-' + '[0-9]+' + '/'
+            repl = ''
+            removeURL = re.sub(pattern, repl, fullnameFile)
+            filePath = "Downloads" + str("/") + removeURL
+
+            open(filePath, "wb").write(response.content)
+
+            print(str(filePath) + "has been downloaded.")
+
+        else:
+            print("This webpage returns a status code of 200, but isn't a D10 spreadsheet or pdf.")
 
     elif response.status_code == 404:
         print("This file is not available.")
