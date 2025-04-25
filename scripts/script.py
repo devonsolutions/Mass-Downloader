528647eef633bdb8ef1223a9f2c19184aee6c92e
CacheOutDev <cache-out@outlook.com>
2025-04-25 14:25:14 -0700

    Moved files out of assets folder

---
+++ scripts/script.py
@@ -0,0 +1,35 @@
+import requests
+import re
+import certifi
+
+with open("scripts/input.txt", "r") as textFile:
+    lines = textFile.readlines()
+
+files = []
+
+for line in lines:
+    file = line.strip()
+    files.append(file)
+
+for file in files:
+
+    response = requests.get(file, verify=certifi.where())
+
+    url = "https://www.csustan.edu/sites/default/files/"
+    file = file.replace(url, "")
+
+    file = re.sub(r"\d+|/", "", file)
+    file = file.replace("-", "")
+
+    if response.status_code == 200:
+
+        file = "downloads/" + file
+        # open(file, "wb").write(response.content)
+
+        print("Downloaded: " + str(file))
+
+    elif response.status_code == 404:
+        print("404 Error: " + file)
+
+    else:
+       print(response.status_code + ": " + file)
